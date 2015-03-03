36f4288732beaf9e14b3e1f03aebfdd36a9fefb7
Eero Kirjavainen <eKirjavainen@gmail.com>
2015-03-03 13:55:27 +0200

    Rocket controls to rotate car while in air for neat 720 tricks

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class RocketController : MonoBehaviour {
     public float rocketAcceleration;
-    Vector3 force = new Vector3();
+    private WheelCollider[] wheels;
+
+    void Start() {
+        wheels = GetComponentsInChildren<WheelCollider>();
+    }
 
 	void FixedUpdate () {
-        float hor = - Mathf.Clamp(Input.GetAxis("RocketHorizontal"), -1, 1);
-        float ver = - Mathf.Clamp(Input.GetAxis("RocketVertical"), -1, 1);
+        StabilizationRockets();
+        RotationRockets();
+	}
+
+    void StabilizationRockets() {
+        float hor = -Mathf.Clamp(Input.GetAxis("RocketHorizontal"), -1, 1);
+        float ver = -Mathf.Clamp(Input.GetAxis("RocketVertical"), -1, 1);
+        Vector3 force = new Vector3();
         force.Set(ver, 0, hor);
+        addRocketForce(force);
+    }
+
+    void RotationRockets() {
+        if (inAir()) {
+            float hor = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1);
+            Vector3 force = new Vector3();
+            force.Set(0, hor, 0);
+            addRocketForce(force);
+        }        
+    }
+
+    void addRocketForce(Vector3 force) {
         force *= rocketAcceleration * Time.deltaTime;
         rigidbody.AddRelativeTorque(force, ForceMode.VelocityChange);
-	}
+    }
+
+    bool inAir() {
+        foreach (WheelCollider col in wheels) {
+            if (col.isGrounded) {
+                return false;
+            }            
+        }
+        return true;
+    }
 }
