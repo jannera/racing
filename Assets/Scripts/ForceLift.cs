b0779e7644d4410b44e768b5e96a75bb65f1602d
gilead <janne.rasanen@iki.fi>
2015-05-02 17:15:05 +0300

    Jump pads inside the house

---
+++ Assets/Scripts/ForceLift.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceLift : MonoBehaviour
+{
+
+    public float metersPerSecond;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody)
+        {
+            other.attachedRigidbody.AddForce(Vector3.up * metersPerSecond, ForceMode.VelocityChange);
+        }
+    }
+}
